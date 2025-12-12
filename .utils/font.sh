e6b06ee69d7f4ab29a957324fffcf42374ab6f94
Sahil Narain <sahil.narain@gmail.com>
2025-12-12 12:21:55 +0000

    Updated font util to use ash syntax

--- .utils/font.sh
+++ .utils/font.sh
@@ -1,10 +1,10 @@
 #!/bin/ash
 
 mount /dev/mmcblk0p1
-fonts=$(ls /mnt/mmcblk0p1/custom/fonts/)
-index=1
+set -- `ls /mnt/mmcblk0p1/custom/fonts`
 
-for font in $fonts
+index=1
+for font in `ls /mnt/mmcblk0p1/custom/fonts/`
 do
   echo $index : $font
   index=$((index+1))
@@ -19,19 +19,14 @@ then
   exit
 fi
 
-index=1
-for font in $fonts
-do
-  if [[ $index -eq $option ]]
-  then
-    echo "Setting font: $font"
-    loadfont < /mnt/mmcblk0p1/custom/fonts/$font
-    echo "Done"
-    sleep 1
-    clear
-  fi
-  index=$((index+1))
-done
+eval "font=\$$option"
 
+echo $font
+
+echo "Setting font: $font"
+loadfont < /mnt/mmcblk0p1/custom/fonts/$font
+echo "Done"
+sleep 1
+clear
 
 umount /mnt/mmcblk0p1
