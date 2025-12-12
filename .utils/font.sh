104f58c4deeeace265df178e1d1d4d772df1ac30
Sahil Narain <sahil.narain@gmail.com>
2025-12-12 13:55:24 +0000

    Improved TUI for font picker

--- .utils/font.sh
+++ .utils/font.sh
@@ -1,14 +1,17 @@
 #!/bin/ash
 
+clear
 mount /dev/mmcblk0p1
 set -- `ls /mnt/mmcblk0p1/custom/fonts`
 
+echo "Pick font #: "
 index=1
 for font in `ls /mnt/mmcblk0p1/custom/fonts/`
 do
   echo $index : $font
   index=$((index+1))
 done
+echo
 
 read option
 
@@ -26,6 +29,7 @@ echo $font
 echo "Setting font: $font"
 loadfont < /mnt/mmcblk0p1/custom/fonts/$font
 echo "Done"
+echo
 sleep 1
 clear
 
